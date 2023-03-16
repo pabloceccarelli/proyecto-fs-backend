b503ce85d6c736f12c8855ea8e21ab8763fd7c10
pablo ceccarelli <pabloceccarelli99@gmail.com>
2023-03-16 19:40:13 -0300

    fijar ip para mostrar en distintos dispositivos

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -3,6 +3,7 @@ const {getProductService} = require("../service/productService");
 const getProductController = async (req, res) => {
     try {
         const response = await getProductService();
+        console.log(response);
         res.send(response);
     } catch (error) {
 
--- src/model/Product.js
+++ src/model/Product.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    idProd: {
+    idProd:{
         type: String,
         required: true
     },
-    marca: {
+    marca:{
         type: String,
         require: true
     },
